0ed28dbb9966c6a8afef6d9f301051d70360f944
Yashvardhan Arora <yash22arora@gmail.com>
2021-10-14 18:31:08 +0530

    Sort arrayOfDivs

--- client/src/components/Array/Array.js
+++ client/src/components/Array/Array.js
@@ -1,5 +1,6 @@
 import styles from './Array.module.css';
 import bubble from '../Sorting/Algo/bubble';
+import { useState } from 'react';
 
 const Array = (props) =>{
 
@@ -7,26 +8,30 @@ const Array = (props) =>{
     let arr=[],i;
     for(i=0 ;i < 50 ;i++) //add props.size
     {
-        let x= Math.floor( Math.random() * 200);
+        let x= Math.floor( Math.random() * 400);
         arr[i]=x;
     }
 
-    //height swap function
-    const swap = (a,b) =>{
-        const s1= getComputedStyle(a).getPropertyValue("height");
-        const s2= getComputedStyle(b).getPropertyValue("height");
-        a.style.height=s2;
-        b.style.height=s1;
-    } 
 
+    let arrayOfDivs = arr.map((a,index)=>{
+        return(
+            <div className={styles.bar}
+            key={index}
+            style={{height:`${a}px`,
+            background:'black'}}>
+            </div>
+        )
+    });    
+
+    
     if(props.algoChoice === 1)
     {
-        bubble(arr);
+        bubble(arrayOfDivs);
     }
 
     return (
         <div className={styles.bars}>
-        {
+        {/* {
          
             arr.map((a)=>{
                 return(
@@ -35,7 +40,10 @@ const Array = (props) =>{
                     </div>
                 )
             })     
-        }            
+        }             */}
+        {
+            arrayOfDivs
+        }
         </div>
     );
 }
--- client/src/components/Sorting/Algo/bubble.js
+++ client/src/components/Sorting/Algo/bubble.js
@@ -1,18 +1,27 @@
-const bubble = (arr) =>{
+import heightExtract from "../../Array/heightExtract";
+const bubble =(arr) =>{
     
-    let i,j;
+    let i,j; 
+    let prev, next;
     for(i=0;i<arr.length-1;i++)
     {
         for(j=0; j<arr.length-i-1; j++)
         {
-            if(arr[j+1]>arr[j])
+            
+            [prev, next] = heightExtract(arr[j], arr[j+1]);
+            
+            if(next>prev)
             {             
+
                 let t=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=t;
+                
             }
         }
+        // console.log(arr[i])
     }
+    return arr;
 }
 
 export default bubble;
